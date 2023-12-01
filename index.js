24085d5d612796d26a832adecb9b5afd44e613b8
Kapin Patel <103712227+KapinPatel6261221355@users.noreply.github.com>
2023-12-01 23:22:35 +0530

    first Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const endDate="01 Dec 2023  11:18 PM"
+
+document.getElementById("enddate").innerHTML=endDate;
+const input=document.querySelectorAll('input')
+const clock=()=>{
+    const end = new Date(endDate)
+    const now = new Date()
+    const diff=(end - now)/1000
+    if(diff<0) return;
+    input[0].value=Math.floor(diff / 3600 / 24)
+    input[1].value=Math.floor(diff / 3600)% 24;
+    input[2].value=Math.floor(diff/60)%60;
+    input[3].value=Math.floor(diff)%60;
+}
+clock()
+
+setInterval(
+    ()=>{
+        clock()
+    },1000
+)
